52c025764906b205fbab6ae8d075835955ee7497
leninrkb <rkb.lenin@gmail.com>
2023-02-17 19:50:25 -0500

    suma

---
+++ holamundo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main incia la ejecucion del programa
+*/
+int main(){
+	printf("hola mundo desde c\nun lenguaje mas a mi cv xd?");
+	return 0;
+}
---
+++ suma.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	int n1, n2;
+	printf("primer entero: ");
+	scanf("%d",&n1);
+	printf("segundo entero: ");
+	scanf("%d",&n2);
+	printf("resultado entero: %d\n",(n1+n2));
+
+}
